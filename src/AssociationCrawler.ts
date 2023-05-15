8d2de712fef66baeda5f7e78def3ed26ab08cbaf
Dan Biagini <28396789+danbiagini@users.noreply.github.com>
2023-05-15 22:50:00 +0000

    add initial test page for crawler

---
+++ src/AssociationCrawler.ts
@@ -0,0 +1,28 @@
+import cheerio from 'cheerio';
+import { loadPage } from './loader';
+import fs = require('fs');
+import { Association, AssociationFilter } from './AssociationScraper';
+
+let DEBUG = false;
+
+export class AssociationCrawler {
+    userAgent?: string;
+    scrapedTeams: Association[];
+    filter?: AssociationFilter;
+    avgDelaySecs?: number;
+
+    constructor(associations: Association[], ua?: string, filter?: AssociationFilter, delay?: number) {
+        this.scrapedTeams = associations;
+
+        if (filter) this.filter = filter;
+        if (delay) this.avgDelaySecs = delay;
+    }
+
+    async crawl(start?: number, count?: number) {
+        let i = start || 0;
+        let last_i = (count ? i + count : this.scrapedTeams.length - 1);
+        while {
+            
+        }
+    }
+}
